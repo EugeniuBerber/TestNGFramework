fcf82dd891081bf8b5b0c99386e19977865b76e2
EugeniuBerber <75701425+EugeniuBerber@users.noreply.github.com>
2021-10-17 13:10:44 -0600

    fist setup version

---
+++ src/test/java/utils/CommonMethods.java
@@ -0,0 +1,46 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class CommonMethods {
+    public static WebDriver driver;
+
+    @BeforeMethod(alwaysRun = true)
+    public void openBrowser() {
+        ConfigReader.readProperties(Constants.CONFIGURATION_FILEPATH);
+        switch (ConfigReader.getPropertyValue("browser")) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+
+            default:
+               throw  new RuntimeException("Invalid browser name");
+        }
+        driver.get(ConfigReader.getPropertyValue("url"));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        if (driver != null)
+            driver.quit();
+    }
+}
---
+++ src/test/java/utils/Constants.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class Constants {
+    public static final String CONFIGURATION_FILEPATH = System.getProperty("user.dir")+"config/config.properties";
+    public static final int IMPLICIT_WAIT = 20;
+}
